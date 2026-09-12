055da08731060a042afc269f64923cc3ad396817
Luna <rylanhughey@gmail.com>
2026-09-12 13:13:27 -0500

    Finished classes.

--- Taskagotchi/Taskagotchi/Taskagotchi.py
+++ Taskagotchi/Taskagotchi/Taskagotchi.py
@@ -1,30 +1,24 @@
 import psutil
 import time
 
-int CPUCheckLength;
-CPUCheckLength = input("How long should each check be: ")
-int CPUCheckInterval;
-CheckInterval = input("How often should it check: ")
-bool Monitoring = true;
+cpuLen= 0
+cpuLen= input("How long should each check be: ")
+cpuInt = 0
+cpuInt = input("How often should it check: ")
+Monitoring = true
 
-if(CPUCheckLength > CheckInterval)
-{
-    CPUCheckLength = CheckInterval;
-    }
+if(cpuLen > cpuInt):
+    cpuLen = cpuInt
+
 
 def check_CPUusage(CPUL)
     return (psutil.cpu_percent(CPUL))
 
 def check_MainFunc()
-{
-        CpuUse = check_CPUusage(CPUCheckLength);
-        print(CpuUse);
+    CpuUse = check_CPUusage(cpuLen)
+    print(CpuUse)
     
-    }
+while (Monitoring == true):
+    time.sleep(cpuInt)
+    check_MainFunc()
 
-while (Monitoring == true)
-{
-    time.sleep(CheckInterval);
-    check_MainFunc();
-
-    }
