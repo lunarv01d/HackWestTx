053f9bae4950c0bf731ecdb81b389f3b4d81b1b1
Mitoaster <pretzelbean13@gmail.com>
2026-09-12 11:57:39 -0700

    Added Memeory and disk

--- Taskagotchi/Taskagotchi/Taskagotchi.py
+++ Taskagotchi/Taskagotchi/Taskagotchi.py
@@ -2,7 +2,7 @@ import psutil
 import time
 
 CPUCheckLength = 0
-CPUCheckLength = int(input("How long should each check be: "))
+CPUCheckLength = int(input("How long should each CPU check be: "))
 CPUCheckInterval = 0
 CheckInterval = int(input("How often should it check: "))
 Monitoring = True
@@ -13,10 +13,23 @@ if(CPUCheckLength > CheckInterval):
 def check_CPUusage(CPUL):
     return (psutil.cpu_percent(CPUL))
 
+def check_MemoryUsage():
+    return ((psutil.virtual_memory().used / 1000000000)) #bytes to Gigabytes
+
+def check_DiskUsage():
+    return ((psutil.disk_usage('/').used / 1000000000)) #bytes to Gigabytes
+
 def check_MainFunc():
     CpuUse = check_CPUusage(CPUCheckLength)
-    print(CpuUse)
+    print(CpuUse, " CPU")
+    MemUse = check_MemoryUsage()
+    print(MemUse, " Memory")
+    DiskUse = check_DiskUsage()
+    print(DiskUse, " Disk")
 
 while (Monitoring == True):
     time.sleep(CheckInterval)
     check_MainFunc()
+
+if _name_ == "_main_":
+    main()
