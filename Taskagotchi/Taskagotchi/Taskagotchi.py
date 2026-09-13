878d5afdca0e81432f1e48e0fa86741dd0f391ee
Luna <rylanhughey@gmail.com>
2026-09-12 20:52:17 -0500

    Something

--- Taskagotchi/Taskagotchi/Taskagotchi.py
+++ Taskagotchi/Taskagotchi/Taskagotchi.py
@@ -35,6 +35,7 @@ def check_DiskUsage():
 def check_DiskRatio():
     return psutil.disk_usage('/').percent  # Ratio of used to total
 
+.
 def check_BatteryPercent():
     return psutil.sensors_battery().percent
 
